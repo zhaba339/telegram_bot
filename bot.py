ec8577811e62c3ebcc3d618625b752a25dd10330
Alex Galinurov <sgalinurov@mail.ru>
2024-06-10 00:44:44 +0600

    all files project

---
+++ bot.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="7001928591:AAFlpoHiF_JVehv6A6cxYp8BrUVHSDXMudw")
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Хэндлер на команду /test1
+@dp.message(Command("test1"))
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+# Хэндлер на команду /test2
+async def cmd_test2(message: types.Message):
+    await message.reply("Test 2")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+dp.message.register(cmd_test2, Command("test2"))
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
