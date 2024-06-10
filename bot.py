30054bddcc6707c2482792282217ab09ffad7f95
Alex Galinurov <sgalinurov@mail.ru>
2024-06-10 22:15:23 +0600

    gfg

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from datetime import datetime
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -9,6 +10,8 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token="7001928591:AAFlpoHiF_JVehv6A6cxYp8BrUVHSDXMudw")
 # Диспетчер
 dp = Dispatcher()
+dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
+dp.start_polling(bot, mylist=[1, 2, 3])
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
@@ -16,20 +19,27 @@ async def cmd_start(message: types.Message):
     await message.answer("Hello!")
 
 # Хэндлер на команду /test1
-@dp.message(Command("test1"))
-async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
+@dp.message(Command("addtolist"))
+async def cmd_addtolist(message: types.Message, mylist: list[int]):
+    mylist.append(7)
+    await message.answer("Добавлено число 7")
 
 # Хэндлер на команду /test2
-async def cmd_test2(message: types.Message):
-    await message.reply("Test 2")
+@dp.message(Command("showlist"))
+async def cmd_showlist(message: types.Message, mylist: list[int]):
+    await message.answer(f"Мой лист {mylist}")
+
+@dp.message(Command("info"))
+async def cmd_dice(message: types.Message, started_at: str):
+    await message.answer(f"Бот запущен {started_at}")
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
 
-dp.message.register(cmd_test2, Command("test2"))
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
 
